d20c8499445f2d9cb93290472d70f1fa027788c6
unknown <c.lalanne92@gmail.com>
2020-02-29 17:18:02 -0300

    add

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email, edad) VALUES ('andres','$2a$10$ykhXmCAam5FUEF9GN.4Z8OwwWJidvMii6VFYe77cmS2X6oF6p4W86',1, 'Andres', 'Guzman','profesor@bolsadeideas.com',56);
 INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email, edad) VALUES ('admin','$2a$10$qGyDfZLBB.SgLv7GCP3uZe3oM38fVtr58T1iZ1LNOvO61loNUAAaK',1, 'John', 'Doe','jhon.doe@bolsadeideas.com', 50);
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email, edad) VALUES ('camilo','$2a$10$ykhXmCAam5FUEF9GN.4Z8OwwWJidvMii6VFYe77cmS2X6oF6p4W86',1, 'Camilo', 'Lalanne','c.lalanne92@gmail.com',27);
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email, edad) VALUES ('camilo','$2a$10$qG7esQiLexWT7R.A9Hvu1utqudjtBRd2VGA5n6voUMslF7u2qYcXW',1, 'Camilo', 'Lalanne','c.lalanne92@gmail.com',27);
 
 INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
 INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
